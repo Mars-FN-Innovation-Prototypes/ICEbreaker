47806f89580620dbe3385a33347685635d97b456
JmanCandyMan <eschmidt.jj@gmail.com>
2026-09-11 12:45:45 -0500

    Deploy Phase 1 testing release from 59f0cc5

---
+++ assets/layout-segment-context-C4hn8J-6.js
@@ -0,0 +1,1 @@
+import{i as e}from"./rolldown-runtime-DWdDZTNf.js";import{i as t}from"./framework-Dykh5YIU.js";import{t as n}from"./index-Bw3v9LhE.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
